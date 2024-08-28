d16f567537a197884df28cfceab6cec5e27d7336
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-08-28 23:32:49 +0200

    Added working topbar with navigation and current path indication

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello world</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
-    </main>
+    <>
+      <main>
+        <h1>Hello world</h1>
+        <Link href="/users">Users</Link>
+        <ProductCard />
+      </main>
+    </>
   );
 }
