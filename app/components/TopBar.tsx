75284eb2dccc6ff1886a8fa32c942edf64a5aa92
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-08-29 21:15:15 +0200

    Added navbar dropdown on sm

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -2,9 +2,11 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import React from "react";
+import React, { useState } from "react";
 
-const TopBar = () => {
+export default function TopBar() {
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
   const currentPath = usePathname();
   const pages = [
     { name: "Home", path: "/" },
@@ -15,23 +17,21 @@ const TopBar = () => {
   };
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+    <nav className="border-gray-200 bg-gray-900">
+      <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto ">
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white p-4">
           NextJS
         </span>
 
         <button
-          data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
+          className="inline-flex items-center m-3 p-2 w-10 h-10 justify-center text-sm rounded-lg sm:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
+          onClick={() => {
+            setIsDropdownVisible(!isDropdownVisible);
+          }}
         >
-          <span className="sr-only">Open main menu</span>
           <svg
-            className="w-5 h-5"
-            aria-hidden="true"
+            className="w-8 h-8"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 17 14"
@@ -45,16 +45,22 @@ const TopBar = () => {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div
+          className={
+            "w-full sm:block sm:w-auto p-4 md:bg-gray-900 " +
+            (isDropdownVisible ? "bg-gray-800" : "hidden")
+          }
+        >
+          <ul className="font-medium flex flex-col md:p-0 sm:flex-row sm:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
             {pages.map((page, index) => (
               <li key={index}>
                 <Link
                   className={
-                    "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent " +
-                    (isActive(page.path) ? "md:text-blue-500" : "md:text-white")
+                    "block py-2 px-3 text-white rounded md:bg-transparent " +
+                    (isActive(page.path) ? "text-blue-500" : "mtext-white")
                   }
                   href={page.path}
+                  onClick={() => setIsDropdownVisible(!isDropdownVisible)}
                 >
                   {page.name}
                 </Link>
@@ -65,6 +71,4 @@ const TopBar = () => {
       </div>
     </nav>
   );
-};
-
-export default TopBar;
+}
